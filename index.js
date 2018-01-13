61082b75fc2c759fb1cde1385f8a72da3dfe1080
laino <lain@volafile.io>
2018-01-13 17:31:17 +0100

    cli: daemon version check

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
-module.exports = {
+const fs = require('fs');
+const path = require('path');
+const version = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, 'package.json')).toString()
+).version;
+
+Object.assign(module.exports, {
     daemon: require('./lib/daemon'),
     client: require('./lib/client'),
-    config: require('./lib/config')
-};
+    config: require('./lib/config'),
+    version
+});
