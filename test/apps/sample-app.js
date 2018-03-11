a4357289b7211884f1a08784956ded5f6ed07c33
laino <nils@volafile.org>
2018-03-11 13:56:56 +0100

    fix test failing with ready-on: instant when server isn't listening yet

--- test/apps/sample-app.js
+++ test/apps/sample-app.js
@@ -1,12 +1,13 @@
 // sample-app.js
 const cluster = require('cluster');
-
+let listening = false;
 const server = require('http').createServer((req, res) => {
     res.end(process.argv.join(' ')); // Reply with process arguments
 }).listen(3334, (error) => {
     if (error) {
         throw error;
     }
+    listening = true;
     process.send('ready');
 });
 
@@ -18,6 +19,9 @@ process.on('message', (msg) => {
 });
 
 function stop() {
+    if (!listening) {
+        process.exit(0);
+    }
     if (cluster.worker) {
         cluster.worker.disconnect();
     } else {
